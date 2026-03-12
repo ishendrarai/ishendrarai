121970d70905dcb23c17c2a4590350841e1b1bec
Ishendra Rai <153349510+ishendrarai@users.noreply.github.com>
2026-03-12 17:03:02 +0530

    Refactor chapter and verse selection logic
    Updated random chapter and verse selection to use specific verse counts for each chapter.

--- gita.py
+++ gita.py
@@ -1,8 +1,15 @@
 import requests
 import random
 
-chapter = random.randint(1, 18)
-verse = random.randint(1, 20)
+# Bhagavad Gita chapters with verse count
+chapters = {
+1:47,2:72,3:43,4:42,5:29,6:47,
+7:30,8:28,9:34,10:42,11:55,12:20,
+13:35,14:27,15:20,16:24,17:28,18:78
+}
+
+chapter = random.choice(list(chapters.keys()))
+verse = random.randint(1, chapters[chapter])
 
 url = f"https://bhagavadgitaapi.in/slok/{chapter}/{verse}"
 
